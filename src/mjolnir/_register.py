8a3984bdd58e5b220f1d16b3153e9c931bc8a791
Tom <tomscheeper@gmail.com>
2025-11-17 21:31:44 +0100

    push

--- src/mjolnir/_register.py
+++ src/mjolnir/_register.py
@@ -1,47 +1,92 @@
+import datetime as dt
 from pathlib import Path
 from typing import Callable, Dict, List
+from uuid import uuid4
 
-from grienetsiis import Decoder, openen_json, opslaan_json
+from grienetsiis import Decoder, openen_json, opslaan_json, invoer_validatie
 
 from .enums import ENUM_DICT
 
 
-class RegisterType:
+class BasisType:
     
-    REGISTRATIES = {}
+    # def __init__(
+    #     self,
+    #     id = None,
+    #     ):
+        
+    #     def krijg_ouder(kind):
+    #         for ouder in kind.__bases__:
+    #             if hasattr(ouder, "REGISTRATIES"):
+    #                 return ouder
+    #         else:
+    #             return None
+        
+    #     if self.__class__ is Register:
+    #         return self
+        
+    #     self.id = str(uuid4()) if id is None else id
+        
+    #     generaties = []
+    #     _self = self.__class__
+    #     ouder = krijg_ouder(_self)
+        
+    #     while self.REGISTRATIES is getattr(ouder, "REGISTRATIES", None):
+            
+    #         generaties.append(_self.__name__)
+    #         _self = krijg_ouder(_self)
+    #         ouder = krijg_ouder(_self)
+        
+    #     generaties = list(reversed(generaties))
+        
+    #     if not generaties:
+    #         self.REGISTRATIES[self.id] = self
+    #     else:
+    #         sub_registraties = self.REGISTRATIES
+    #         for kind in generaties[:-1]:
+    #             sub_registraties = sub_registraties.setdefault(kind, {})
+    #         if generaties[-1] not in sub_registraties:
+    #             sub_registraties[generaties[-1]] = {}
+    #         sub_registraties[generaties[-1]][self.id] = self
+    
+    @classmethod
+    def van_json(
+        cls,
+        **dict,
+        ) -> "Register":
+        
+        if "datum" in dict:
+            dict["datum"] = dt.datetime.strptime(dict["datum"], "%Y-%m-%d").date()
+        
+        return cls(**dict)
+    
+    @classmethod
+    def nieuw(
+        cls,
+        velden,
+        ) -> "BasisType":
+        
+        dict = {}
+        
+        for veld, type in velden.items():
+            waarde = invoer_validatie(veld, type)
+            dict[veld] = waarde
+        
+        return cls(**dict)
+
+class Register(dict):
     
     BESTANDSMAP: Path = Path("gegevens")
     EXTENSIE: str = "json"
     
-    DECODER_FUNCTIE: Callable = None
+    DECODER_FUNCTIE: str = "van_json"
     DECODER_LIJST: List[Decoder] = None
     
     ENCODER_FUNCTIE: Callable = None
     ENCODER_DICT: Dict[str, str] = None
     
-    def __init__(
-        self,
-        identifier,
-        ):
-        
-        def set_recursively(register, object, identifier, parent = None):
-            parent = object.__class__.__bases__[0] if parent is None else parent
-            
-            if object.__class__ is RegisterType:
-                register[identifier] = object
-            
-            elif parent is RegisterType:
-                if not object.__class__.__name__ in register:
-                    register[object.__class__.__name__] = {}
-                
-                register[object.__class__.__name__][identifier] = object
-            else:
-                set_recursively(register[parent.__name__], object, identifier, parent.__bases__[0])
-        
-        set_recursively(self.REGISTRATIES, self, identifier)
-    
     @classmethod
-    def openen(cls) -> "RegisterType":
+    def openen(cls) -> "Register":
         
         if not cls.BESTANDSMAP.is_dir():
             cls.BESTANDSMAP.mkdir()
@@ -50,14 +95,12 @@ class RegisterType:
         
         if bestandspad.is_file():
             
-            cls.REGISTRATIES = openen_json(
+            return cls(**openen_json(
                 bestandspad,
-                decoder_functie = cls.DECODER_FUNCTIE,
+                decoder_functie = getattr(cls.TYPE, cls.DECODER_FUNCTIE),
                 decoder_lijst = cls.DECODER_LIJST,
                 enum_dict = ENUM_DICT,
-                )
-            
-            return cls
+                ))
         else:
             return cls()
     
@@ -65,13 +108,19 @@ class RegisterType:
         bestandspad = self.BESTANDSMAP / f"{self.BESTANDSNAAM}.{self.EXTENSIE}"
         
         opslaan_json(
-            self.REGISTRATIES,
+            self,
             bestandspad,
-            encoder_functie = self.ENCODER_FUNCTIE,
+            encoder_functie = getattr(self, "ENCODER_FUNCTIE"),
             encoder_dict = self.ENCODER_DICT,
             enum_dict = ENUM_DICT,
             )
     
     @property
-    def lijst(self) -> List["RegisterType"]:
+    def lijst(self) -> List["Register"]:
         return list(self.values())
+    
+    def nieuw(self):
+        basis_type = self.TYPE.nieuw(self.TYPE.VELDEN)
+        id = str(uuid4())
+        self[id] = basis_type
+        return self
--- src/mjolnir/belading.py
+++ src/mjolnir/belading.py
@@ -3,10 +3,18 @@ from pathlib import Path
 from typing import Callable, List
 
 from .enums import HalterType
-from .gegevensdrager import BasisType, DatabankLijst
-
+# from .gegevensdrager import BasisType, DatabankLijst
+from ._register import BasisType, Register
 
 class Halterschijf(BasisType):
+    
+    VELDEN: dict[str, type] = {
+        "massa": float,
+        "diameter": int,
+        "aantal": int,
+        "breedte": int,
+        # "id": str,
+        }
     
     def __init__(
         self,
@@ -14,6 +22,7 @@ class Halterschijf(BasisType):
         diameter: int,
         aantal: int,
         breedte: int,
+        # id: str = None,
         ) -> "Halterschijf":
         
         self.massa = massa
@@ -21,81 +30,81 @@ class Halterschijf(BasisType):
         self.aantal = aantal
         self.breedte = breedte
 
-class Halterschijven(DatabankLijst):
+class Halterschijven(Register):
     
     BESTANDSNAAM: str = "halterschijven"
-    DECODER_FUNCTIE: Callable = Halterschijf.van_json
+    TYPE: type = Halterschijf
 
-class Halterstang(BasisType):
+# class Halterstang(BasisType):
     
-    def __init__(
-        self,
-        naam: str,
-        halter_type: HalterType,
-        massa: float,
-        diameter: int,
-        opname_breedte: int,
-        ) -> "Halterstang":
+#     def __init__(
+#         self,
+#         naam: str,
+#         halter_type: HalterType,
+#         massa: float,
+#         diameter: int,
+#         opname_breedte: int,
+#         ) -> "Halterstang":
         
-        self.naam = naam
-        self.halter_type = halter_type
-        self.massa = massa
-        self.diameter = diameter
-        self.opname_breedte = opname_breedte
+#         self.naam = naam
+#         self.halter_type = halter_type
+#         self.massa = massa
+#         self.diameter = diameter
+#         self.opname_breedte = opname_breedte
     
-    def laden(
-        self,
-        haltermassa: float,
-        halterschijven: List[Halterschijf] = None,
-        ) -> "Halter":
+#     def laden(
+#         self,
+#         haltermassa: float,
+#         halterschijven: List[Halterschijf] = None,
+#         ) -> "Halter":
         
-        halterschijven = Halterschijven.openen() if halterschijven is None else halterschijven
-        halterschijven.sort(key = lambda x: x.massa, reverse = True)
+#         halterschijven = Halterschijven.openen() if halterschijven is None else halterschijven
+#         halterschijven.sort(key = lambda x: x.massa, reverse = True)
         
-        massa_per_kant_nodig = (haltermassa - self.massa)/2
-        ruimte_over = self.opname_breedte
+#         massa_per_kant_nodig = (haltermassa - self.massa)/2
+#         ruimte_over = self.opname_breedte
         
-        halterschijven_links = []
-        halterschijven_rechts = []
+#         halterschijven_links = []
+#         halterschijven_rechts = []
         
-        for halterschijf in halterschijven:
-            for _ in range(halterschijf.aantal//2):
-                if halterschijf.massa <= round(massa_per_kant_nodig * 0.8)/0.8 and ruimte_over > halterschijf.breedte:
-                    halterschijven_links.append(halterschijf)
-                    halterschijven_rechts.append(halterschijf)
-                    massa_per_kant_nodig -= halterschijf.massa
-                    ruimte_over -= halterschijf.breedte
-                else:
-                    continue
+#         for halterschijf in halterschijven:
+#             for _ in range(halterschijf.aantal//2):
+#                 if halterschijf.massa <= round(massa_per_kant_nodig * 0.8)/0.8 and ruimte_over > halterschijf.breedte:
+#                     halterschijven_links.append(halterschijf)
+#                     halterschijven_rechts.append(halterschijf)
+#                     massa_per_kant_nodig -= halterschijf.massa
+#                     ruimte_over -= halterschijf.breedte
+#                 else:
+#                     continue
         
-        return Halter(
-            self,
-            halterschijven_links,
-            halterschijven_rechts,
-            )
+#         return Halter(
+#             self,
+#             halterschijven_links,
+#             halterschijven_rechts,
+#             )
 
-class Halterstangen(DatabankLijst):
+# class Halterstangen(DatabankLijst):
     
-    BESTANDSNAAM: str = "halterstangen"
-    DECODER_FUNCTIE: Callable = Halterstang.van_json
-    ENCODER_FUNCTIE: Callable = Halterstang.naar_json
+#     BESTANDSNAAM: str = "halterstangen"
+#     DECODER_FUNCTIE: Callable = Halterstang.van_json
+#     ENCODER_FUNCTIE: Callable = Halterstang.naar_json
 
-class Halter:
+# class Halter:
     
-    def __init__(
-        self,
-        halterstang: Halterstang,
-        halterschijven_links: List[Halterschijf] = None,
-        halterschijven_rechts: List[Halterschijf] = None,
-        ) -> "Halter":
+#     def __init__(
+#         self,
+#         halterstang: Halterstang,
+#         halterschijven_links: List[Halterschijf] = None,
+#         halterschijven_rechts: List[Halterschijf] = None,
+#         ) -> "Halter":
         
-        self.halterstang = halterstang
-        self.halterschijven_links = halterschijven_links
-        self.halterschijven_rechts = halterschijven_rechts
+#         self.halterstang = halterstang
+#         self.halterschijven_links = halterschijven_links
+#         self.halterschijven_rechts = halterschijven_rechts
     
-    @property
-    def massa(self) -> float:
+#     @property
+#     def massa(self) -> float:
         
-        return self.halterstang.massa + \
-            sum([halterschijf.massa for halterschijf in self.halterschijven_links]) + \
-            sum([halterschijf.massa for halterschijf in self.halterschijven_rechts])
+#         return self.halterstang.massa + \
+#             sum([halterschijf.massa for halterschijf in self.halterschijven_links]) + \
+#             sum([halterschijf.massa for halterschijf in self.halterschijven_rechts])
