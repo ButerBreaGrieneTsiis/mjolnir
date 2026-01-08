41fac9fc64c9ab1a03fea6ce35b67d2b6b4795ef
TomScheeper <tomscheeper@gmail.com>
2026-01-08 20:36:56 +0100

    header verborgen

--- src/mjolnir/paneel.py
+++ src/mjolnir/paneel.py
@@ -8,6 +8,24 @@ st.set_page_config(layout = "wide")
 
 def paneel():
     
+    st.markdown(
+        f"""
+        <style>
+            .block-container {{
+                padding-top: 0.0rem;
+                padding-bottom: 0rem;
+                padding-left: 2rem;
+                padding-right: 2rem;
+                }}
+            
+            header {{visibility: hidden;}}
+            
+            .main {{overflow: hidden}}
+        </style>
+        """,
+        unsafe_allow_html = True,
+        )
+    
     if "register" not in st.session_state:
         st.session_state["register"] = Register.openen()
     
